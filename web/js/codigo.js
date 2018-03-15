11c085a897937944ea9afec3e2e32711a6ea9f31
Pedro <Pedro@Equipo_1>
2018-03-14 22:52:30 -0500

    Websockets - Chat

---
+++ web/js/codigo.js
@@ -0,0 +1,38 @@
+(function (window, document, JSON) {
+    'use strict';
+    var url = 'ws://' + window.location.host + '/websockets/Chats',
+            ws = new WebSocket(url),
+            mensajes = document.getElementById('conversacion'),
+            boton = document.getElementById('enviar'),
+            nombre = document.getElementById('usuario'),
+            mensaje = document.getElementById('mensaje');
+
+    ws.onopen = onOpen;
+    ws.onclose = onClose;
+    ws.onmessage = onMessage;
+
+    boton.addEventListener('click', enviar);
+
+    function onOpen() {
+        console.log('Conectado...');
+    }
+    function onClose() {
+        console.log('Desconectado...');
+    }
+    function enviar() {
+        var msg = {
+            nombre: nombre.value,
+            mensaje: mensaje.value
+        }
+        ws.send(JSON.stringify(msg));
+    }
+
+    function onMessage(evt) {
+        var obj = JSON.parse(evt.data),
+                msg = 'Nombre: ' + obj.nombre +
+                ' dice:' + obj.mensaje;
+        mensajes.innerHTML += '<br/>' + msg;
+
+    }
+
+})(window, document, JSON);
